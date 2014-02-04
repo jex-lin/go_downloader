d799c775fbf1872ae2e9d38daf23dcb2bbf70aa0
Jex Lin <italwaysrainonme@gmail.com>
2014-02-04 18:51:32 +0800

    Update index.html UI/UX

--- static/js/index.js
+++ static/js/index.js
@@ -1,24 +1,31 @@
 $(document).ready(function(){
     connect_websocket();
-    $("#url1").change(function() {
-        $("#url1-btn-down").removeAttr("disabled");
-        $("#url1-btn-down").removeClass("btn-danger btn-success").addClass("btn-default");
-        $("#url1-btn-down").html("Download");
-        $("#url1-progress").show();
-        $("#url1-progress-bar").css("width", "1%");
-    })
-    $("#url1-btn-down").click(function() {
+
+    $("#url1-download-container").click(function() {
         var data = {
             "Target" : "url1",
             "Url"    : $("#url1").val()
         };
         ws.send(JSON.stringify(data));
-        $("#url1-btn-down").removeClass("btn-default btn-danger btn-success").addClass("btn-warning");
-        $("#url1-btn-down").html("Downloading...");
-        $("#url1-btn-down").attr("disabled", "disabled");
-        $("#url1-progress").removeClass("hide");
+
+        $(this).addClass("hide");
+        $("#url1-wait-container").removeClass("hide");
+        $("#url1-progress-container").removeClass("hide");
     })
 })
+
+function set_list_item_success($this) {
+    $this.removeClass("list-group-item-info");
+    $this.removeClass("list-group-item-danger");
+    $this.addClass("list-group-item-success");
+}
+
+function set_list_item_error($this) {
+    if ($this.hasClass("list-group-item-info")) {
+        $this.removeClass("list-group-item-info");
+        $this.addClass("list-group-item-danger");
+    }
+}
 
 function connect_websocket() {
     ws = new WebSocket("ws://127.0.0.1:9090/download/");
@@ -34,10 +41,14 @@ function connect_websocket() {
         if (res["Target"] == "url1") {
             if (res["Status"] == "ok") {
                 setInterval(function(){
-                    $("#url1-btn-down").removeClass("btn-warning");
-                    $("#url1-btn-down").addClass("btn-success");
-                    $("#url1-btn-down").html("Success");
-                    $("#url1-progress").addClass("hide");
+                    $("#url1-download-container").addClass("hide");
+                    $("#url1-progress-container").addClass("hide");
+                    $("#url1-status-ok").removeClass("hide");
+                    $("#url1-status-fail").addClass("hide");
+                    $("#url1-wait-container").addClass("hide");
+                    $("#url1-play-container").removeClass("hide");
+                    $("#url1").attr("disabled", "disabled");
+                    set_list_item_success($("#url1-list-group-item"));
                 }, 1000);
             } else if (res["Status"] == "keep") {
                 if ($("#url1-play-container").hasClass("hide")) {
@@ -46,11 +57,12 @@ function connect_websocket() {
                 $("#url1-progress-bar").css("width", res["Progress"]+"%");
             } else if (res["Status"] == "fail") {
                 $("#url1-play-container").addClass("hide");
-                $("#url1-btn-down").removeClass("btn-warning");
-                $("#url1-btn-down").removeAttr("disabled");
-                $("#url1-btn-down").addClass("btn-danger");
-                $("#url1-progress").addClass("hide");
-                $("#url1-btn-down").html("Retry");
+                $("#url1-wait-container").addClass("hide");
+                $("#url1-download-container").removeClass("hide");
+                $("#url1-progress-container").addClass("hide");
+                $("#url1-status-ok").addClass("hide");
+                $("#url1-status-fail").removeClass("hide");
+                set_list_item_error($("#url1-list-group-item"));
             }
         }
     }
