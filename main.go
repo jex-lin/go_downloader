707a490a334137bfd673053d311226dee532ee20
jex-lin <jxxxlin@gmail.com>
2015-01-20 21:15:32 +0800

    Add .gitignore, and remove unuseful controller

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-    "code.google.com/p/go.net/websocket"
-    "go_downloader/controller/index"
-    "net/http"
-    "log"
+	"code.google.com/p/go.net/websocket"
+	"go_downloader/controller/index"
+	"log"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", index.Home)
-    http.Handle("/download/", websocket.Handler(index.Download))
-    http.HandleFunc("/playVideo/", index.PlayVideo)
-    http.HandleFunc("/static/", index.Static)
-    http.HandleFunc("/thisav/", index.Thisav)
-    err := http.ListenAndServe(":9111", nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	http.HandleFunc("/", index.Home)
+	http.Handle("/download/", websocket.Handler(index.Download))
+	http.HandleFunc("/playVideo/", index.PlayVideo)
+	http.HandleFunc("/static/", index.Static)
+	err := http.ListenAndServe(":9111", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
